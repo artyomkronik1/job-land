7abf501a4d68553aa225985e06b5d78aa1075597
--add <artiom.co.il>
2024-04-04 12:30:39 +0300

    message ui

--- job-land-frontend/src/components/messages-component/messages-component.tsx
+++ job-land-frontend/src/components/messages-component/messages-component.tsx
@@ -19,22 +19,8 @@ const  MessagesComponent  = observer( ()=>{
     const [openChat, setOpenChat] = useState<Chat>()
     const openNewChat = async(chat:Chat)=>{
         setOpenChat(chat)
-        console.log(openChat)
+        console.log('openChat', openChat)
     }
-    console.log(chats[0])
-
-    // async function getAllChatsByUser() {
-    //    const result = await UserStore.getChatsByUser("artiom")
-    //     if(result.success){
-    //         setChats(result.chats);
-    //         console.log('chats',chats)
-    //     }
-    //
-    // }
-    // useEffect(() => {
-    //     getAllChatsByUser();
-    //
-    // }, []);
     return (
         <>
             <div dir={ UserStore.getLanguage()=='en'?'ltr':'rtl'}>
@@ -44,50 +30,45 @@ const  MessagesComponent  = observer( ()=>{
                     {chats.length>0?chats.map((chat:Chat, index)=>
                         <div className={styles.messagesContainer} key={index} onClick={()=>openNewChat(chat)} >
                             <div className={styles.messagesContainer__leftSide}>
-                                <div style={{ paddingLeft:'20px', paddingBottom:'20px', borderBottom:'1px solid #cfd0d2',marginBottom:'50px', width:'109%', display:'flex',justifyContent:'start' }}>
-                                <span  style={{color:'#a9acb1', fontSize:'22px'}} className={globalStyles.simpleP}>{t('Messaging')}</span>
-                                </div>
                                     {/*<div className={globalStyles.separate_line_grey} style={{marginBottom:'50px' ,width:'110%' }}></div>*/}
                                 <div className={styles.messagesContainer__leftSide__messageBox}>
                                     <div style={{display:'flex',alignItems:'center', justifyContent:'space-between', width:'100%'  }}>
-                                          <div style={{display:'flex', flexDirection:'row', alignItems:'start'}}>
+                                            <div style={{display:'flex', flexDirection:'row', alignItems:'start', gap:'10px'}}>
                                             <ProfileImage name={chat.messages[0].sender!=UserStore.user.id? UserStore.getUserNameById(chat.messages[0].sender) : UserStore.getUserNameById(chat.messages[0].receiver)}/>
                                             <div style={{display:'flex', flexDirection:'column', gap:'10px'}}>
                                              <span style={{fontSize:'15px', color:'#404141'}} className={globalStyles.simpleP}>{UserStore.user.id!=chat.messages[0].receiver ? UserStore.getUserNameById(chat.messages[0].receiver) :  UserStore.getUserNameById(chat.messages[0].sender)}</span>
                                               <span style={{fontSize:'15px', color:'#404141'}} className={globalStyles.simpleP}>{UserStore.user.id!=chat.messages[0].receiver ? UserStore.getUserInfoById(chat.messages[0].receiver)?.about :  UserStore.getUserInfoById(chat.messages[0].sender)?.about}</span>
                                             </div>
                                           </div>
+
                                               <i className={`fa fa-arrow-circle-right ${styles.arrowIcon} `} aria-hidden="true"></i>
                                     </div>
-                                    {/*<div style={{display:'flex',  justifyContent:'center' }}>*/}
-                                    {/*    <div style={{ width:'100%'}} className={globalStyles.separate_line_grey}></div>*/}
-                                    {/*</div>*/}
+                                    <div style={{  paddingBottom:'20px', borderBottom:'1px solid #cfd0d2',marginBottom:'50px', width:'100%', display:'flex',justifyContent:'start' }}>
+                                    </div>
                                 </div>
+
                             </div>
                             <div className={styles.messagesContainer__rightSide}>
-                                <div className={styles.messagesContainer__rightSide__header} style={{ marginBottom:'50px', paddingLeft:'20px', paddingBottom:'20px', borderBottom:'1px solid #cfd0d2', width:'103%', display:'flex',justifyContent:'start' }}>
-                                    <span  style={{color:'#a9acb1', fontSize:'22px'}} className={globalStyles.simpleP}>{UserStore.user.id!=chat.messages[0].receiver ? UserStore.getUserNameById(chat.messages[0].receiver) :  UserStore.getUserNameById(chat.messages[0].sender)}</span>
-                                </div>
+
                                 <div style={{height:'100%', display:'flex' , flexDirection:'column', alignItems:'start', width:'100%'}}>
                                 {openChat?.messages.map((msg:Message, index)=>
-                                    <div style={{display:'flex' , justifyContent:'space-between', width:'100%', flexDirection:'column', gap:'30px', marginBottom:'30px'}}>
+                                    <div  key={index} style={{display:'flex' , justifyContent:'space-between', width:'100%', flexDirection:'column', gap:'30px', marginBottom:'30px'}}>
                                         {msg.sender==userStore.user.id? (
                                             <div style={{display:'flex', justifyContent:'start', width:'100%', gap:'8px'}}>
                                                 <ProfileImage name={UserStore.user.name}/>
                                                 <div style={{display:'flex',gap:'10px', flexDirection:'column', alignItems:'start', justifyContent:'center'}}>
-                                                    <span style={{fontSize:'18px', color:'#404141'}} className={globalStyles.simpleP}>{UserStore.user.name}</span>
                                                     <span style={{fontSize:'18px',color:'#404141'}} className={globalStyles.simpleP}>{msg.content}</span>
                                                 </div>
                                             </div>
                                         ):
-
+                                    <div style={{display:'flex', flexDirection:'column'}}>
                                         <div style={{display:'flex', justifyContent:'start', width:'100%', gap:'8px'}}>
                                             <ProfileImage name={ UserStore.getUserNameById(msg.sender) }/>
                                            <div style={{display:'flex',gap:'10px', flexDirection:'column', alignItems:'start', justifyContent:'center'}}>
-                                            <span style={{fontSize:'18px',color:'#404141'}} className={globalStyles.simpleP}>{ UserStore.getUserNameById(msg.sender) }</span>
                                             <span style={{fontSize:'18px',color:'#404141'}} className={globalStyles.simpleP}>{msg.content}</span>
                                            </div>
                                         </div>
+                                    </div>
                                         }
                                     </div>
                                 )}
