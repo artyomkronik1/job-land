da01ca1086752936b6812b557beff19bf9f1258c
--add <artiom.co.il>
2024-05-14 23:07:22 +0300

    message component header

--- job-land-frontend/src/components/messages-component/messages-component.tsx
+++ job-land-frontend/src/components/messages-component/messages-component.tsx
@@ -13,6 +13,8 @@ import MessageService from "../../services/messageService";
 import TextAreaComponent from "../../base-components/textArea/text-area-component";
 import Login from "../login/login";
 import {useParams} from "react-router";
+import SearchInput from "../../base-components/search-input/search-input";
+import newmsg from '../../assets/images/newmsg.png'
 const  MessagesComponent  = ()=> {
 
     // get all chats
@@ -59,43 +61,53 @@ const  MessagesComponent  = ()=> {
             <div dir={ UserStore.getLanguage()=='en'?'ltr':'rtl'}>
                 <div style={{ marginTop:'90px',display:'flex', flexDirection:'column', alignItems:'center', width:'100%', height:'100vh'}} >
                 {/*    messages container*/}
-
+                {/*    title*/}
+                    <div style={{backgroundColor:'white' , width:'100%', height:'150px', display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+                        <h1 style={{paddingLeft:'20px', color:'#a9acb1'}}>Messaging</h1>
+                        <img style={{marginRight:'50px', cursor:'pointer'}} width={30} height={30} src={newmsg}/>
+                    </div>
+                    <div style={{  borderBottom:'1px solid #cfd0d2', width:'100%', display:'flex',justifyContent:'start' }}>
+                    </div>
                     {chats.length>0?chats.map((chat:Chat, index)=>
                         <div className={styles.messagesContainer} key={index} onClick={()=>openNewChat(chat)} >
+
                             <div className={styles.messagesContainer__leftSide}>
+
                                     {/*<div className={globalStyles.separate_line_grey} style={{marginBottom:'50px' ,width:'110%' }}></div>*/}
                                 <div className={styles.messagesContainer__leftSide__messageBox}>
-                                    <div style={{display:'flex',alignItems:'center', justifyContent:'space-between', width:'100%'  }}>
-                                            <div style={{display:'flex', flexDirection:'row', alignItems:'start', gap:'10px'}}>
+                                    <div  className={chat._id==openChat?._id? styles.openedChat:styles.closedChat} style={{paddingBottom:'30px', paddingTop:'30px' ,display:'flex',alignItems:'center', justifyContent:'space-between', width:'100%'  }}>
+                                            <div style={{display:'flex', flexDirection:'row', alignItems:'start', gap:'10px', paddingLeft:'10px'}}>
                                             <ProfileImage name={chat.messages[0].sender!=UserStore.user.id? UserStore.getUserNameById(chat.messages[0].sender) : UserStore.getUserNameById(chat.messages[0].receiver)}/>
-                                            <div style={{display:'flex', flexDirection:'column', gap:'10px'}}>
+                                            <div style={{display:'flex', flexDirection:'column', gap:'10px', alignItems:'start'}}>
                                              <span style={{fontSize:'15px', color:'#404141'}} className={globalStyles.simpleP}>{UserStore.user.id!=chat.messages[0].receiver ? UserStore.getUserNameById(chat.messages[0].receiver) :  UserStore.getUserNameById(chat.messages[0].sender)}</span>
+                                                                <span style={{fontSize:'13px', fontWeight:'normal', color:'#79797a'}} className={globalStyles.simpleP}>{UserStore.user.id!=chat.messages[0].receiver ? UserStore.getUserInfoById(chat.messages[0].receiver)?.about :  UserStore.getUserInfoById(chat.messages[0].sender)?.about}</span>
+
                                             </div>
                                           </div>
 
                                     </div>
-                                    <div style={{  paddingBottom:'20px', borderBottom:'1px solid #cfd0d2',marginBottom:'50px', width:'100%', display:'flex',justifyContent:'start' }}>
+                                    <div style={{  borderBottom:'1px solid #cfd0d2', width:'100%', display:'flex',justifyContent:'start' }}>
                                     </div>
                                 </div>
 
                             </div>
                             <div className={styles.messagesContainer__rightSide}>
                                 <div style={{ display:'flex' , flexDirection:'column', alignItems:'start', width:'100%'}}>
-                                    {/*title of the chat*/}
-                                    {openChat ? (
-                                        <div style={{display:'flex',alignItems:'start', justifyContent:'space-between', width:'100%' ,flexDirection:'column'  }}>
-                                            <div style={{display:'flex', flexDirection:'row', alignItems:'start', gap:'10px'}}>
-                                                <div style={{display:'flex', flexDirection:'column', gap:'10px', alignItems:'start'}}>
-                                                    <span style={{fontSize:'15px', color:'#404141'}} className={globalStyles.simpleP}>{UserStore.user.id!=openChat.messages[0].receiver ? UserStore.getUserNameById(openChat.messages[0].receiver) :  UserStore.getUserNameById(openChat.messages[0].sender)}</span>
-                                                    <span style={{fontSize:'13px', fontWeight:'normal', color:'#79797a'}} className={globalStyles.simpleP}>{UserStore.user.id!=openChat.messages[0].receiver ? UserStore.getUserInfoById(openChat.messages[0].receiver)?.about :  UserStore.getUserInfoById(openChat.messages[0].sender)?.about}</span>
+                                    {/*/!*title of the chat*!/*/}
+                                    {/*{openChat ? (*/}
+                                    {/*    <div style={{display:'flex',alignItems:'start', justifyContent:'space-between', width:'100%' ,flexDirection:'column'  }}>*/}
+                                    {/*        <div style={{display:'flex', flexDirection:'row', alignItems:'start', gap:'10px'}}>*/}
+                                    {/*            <div style={{display:'flex', flexDirection:'column', gap:'10px', alignItems:'start'}}>*/}
+                                    {/*                <span style={{fontSize:'15px', color:'#404141'}} className={globalStyles.simpleP}>{UserStore.user.id!=openChat.messages[0].receiver ? UserStore.getUserNameById(openChat.messages[0].receiver) :  UserStore.getUserNameById(openChat.messages[0].sender)}</span>*/}
+                                    {/*                <span style={{fontSize:'13px', fontWeight:'normal', color:'#79797a'}} className={globalStyles.simpleP}>{UserStore.user.id!=openChat.messages[0].receiver ? UserStore.getUserInfoById(openChat.messages[0].receiver)?.about :  UserStore.getUserInfoById(openChat.messages[0].sender)?.about}</span>*/}
 
-                                                </div>
-                                            </div>
-                                            <div style={{  paddingBottom:'20px', borderBottom:'1px solid #cfd0d2',marginBottom:'50px', width:'100%', display:'flex',justifyContent:'start' }}>
-                                            </div>
-                                        </div>
+                                    {/*            </div>*/}
+                                    {/*        </div>*/}
+                                    {/*        <div style={{  paddingBottom:'20px', borderBottom:'1px solid #cfd0d2',marginBottom:'50px', width:'100%', display:'flex',justifyContent:'start' }}>*/}
+                                    {/*        </div>*/}
+                                    {/*    </div>*/}
 
-                                    ):null}
+                                    {/*):null}*/}
                                     {/*messages*/}
                                     <div style={{display:'flex', flexDirection:'column', width:'100%', maxHeight:'700px', overflowY:'scroll'}}>
                                     {openChat?.messages.map((msg:Message, index)=>
