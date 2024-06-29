a1c2f1d069db3a3f7922dd13365ef2adbb58f936
--add <artiom.co.il>
2024-06-29 04:19:05 +0300

    login and sign up gap

--- job-land-frontend/src/components/login/login.tsx
+++ job-land-frontend/src/components/login/login.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import TextInputField from "../../base-components/text-input/text-input-field";
 import styles from './login.module.scss'
 import globalStyles from '../../assets/global-styles/styles.module.scss'
@@ -9,45 +9,44 @@ import he from '../../assets/images/languages/he.png'
 import googleIcon from '../../assets/images/social-media/google.png'
 import facebookIcon from '../../assets/images/social-media/facebook.png'
 import UserStore from '../../store/user';
-import {observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 import ToastComponent from '../../base-components/toaster/ToastComponent';
-import {toast} from "react-toastify";
-import {useNavigate} from "react-router";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
 import CryptoJS from "crypto-js";
 import loginPicture from '../../assets/images/login.png'
 import Spinner from "../../base-components/loading-spinner/loading-spinner";
-const  Login  = observer( ()=>{
+const Login = observer(() => {
     //language
     const { t } = useTranslation();
     const { i18n } = useTranslation();
-    const changeLanguage = (lng:string) => {
+    const changeLanguage = (lng: string) => {
         UserStore.setLanguage(lng)
         i18n.changeLanguage(lng);
     };
     const navigate = useNavigate();
     //password
     const [userPassword, setUserPassword] = useState('');
-    const handleInputChangePassword = (value:string) => {
+    const handleInputChangePassword = (value: string) => {
         setUserPassword(value);
     };
 
     //email
     const [userEmail, setUserEmail] = useState('');
-    const handleInputChangeEmail = (value:string) => {
+    const handleInputChangeEmail = (value: string) => {
         setUserEmail(value);
     };
     const secretKey = 'job-land'; //  secret key
-    const resetParameter=()=>{
+    const resetParameter = () => {
         setUserEmail('')
         setUserPassword('')
     }
 
     //login
-    const login=async (event: any) => {
+    const login = async (event: any) => {
         event.preventDefault();
-        if( userEmail.length==0 || userPassword.length==0)
-        {
-            toast.error('ERROR! One or more fields is empty' );
+        if (userEmail.length == 0 || userPassword.length == 0) {
+            toast.error('ERROR! One or more fields is empty');
         }
         const encryptedPassword = CryptoJS.AES.encrypt(userPassword, secretKey).toString();
         const res = await UserStore.login(userEmail, encryptedPassword)
@@ -55,17 +54,17 @@ const  Login  = observer( ()=>{
             resetParameter()
             UserStore.setLoading(true);
             toast.success(t('SUCCESS'));
-             setTimeout(() => {
+            setTimeout(() => {
                 UserStore.setLoading(false);
-                 UserStore.setSessionKey(res.session_key)
-                 navigate('/')
-             }, 3000);
+                UserStore.setSessionKey(res.session_key)
+                navigate('/')
+            }, 3000);
         } else {
             toast.error(t('ERROR') + ' ' + res?.errorCode);
         }
     }
 
-    const goSignUp =()=>{
+    const goSignUp = () => {
         navigate('/signup')
         UserStore.setSignedUp(false)
         UserStore.setLoggedIn(false)
@@ -75,48 +74,50 @@ const  Login  = observer( ()=>{
 
         <>
             <ToastComponent />
-        <form className={styles.form} dir={ UserStore.getLanguage()=='en'?'ltr':'rtl'}>
-            {/*toast*/}
-            <ToastComponent />
-            {/*header*/}
-            <div className={styles.formHeader}>
-                <img src={icon}  className={globalStyles.logoPic}/>
-                <div style={{display:'flex', flexDirection:'column'}}>
-                    <h2 className={styles.title} > {t('Welcome to your professional space' )} <br/>{t('to find a new job' )} </h2>
-                </div>
-                <div className={styles.languageDiv}>
-                    { UserStore.getLanguage()=='en' ?
-                        <img src={he} className={styles.heLanguagePic} onClick={()=>changeLanguage('he')}/>
-                        :
-                        <img src={en} className={styles.enlanguagePic}  onClick={()=>changeLanguage('en')}/>
-                    }
-                </div>
-            </div>
-            {/*body*/}
-            <div className={styles.body}>
-                <div className={styles.textForm}>
-                    <TextInputField type={'text'} placeHolder={t('Enter Your Email')} text={t('Email')} value={userEmail} onChange={handleInputChangeEmail}/>
-                    <TextInputField type={'text'} placeHolder={t('Enter Your Password')} text={t('Password')} value={userPassword} onChange={handleInputChangePassword}/>
-                    <a className={globalStyles.mainSpan}>{t('Forgot your password?')}</a>
-                    <div style={{marginTop:"15px", display:"flex", justifyContent:"center"}}>
-                        <button className={globalStyles.btn} onClick={login}> {t('Sign In') }</button>
+            <form className={styles.form} dir={UserStore.getLanguage() == 'en' ? 'ltr' : 'rtl'}>
+                {/*toast*/}
+                <ToastComponent />
+                {/*header*/}
+                <div className={styles.formHeader}>
+                    <img src={icon} className={globalStyles.logoPic} />
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <h2 className={styles.title} > {t('Welcome to your professional space')} <br />{t('to find a new job')} </h2>
                     </div>
-                    <div style={{marginTop:"40px"}} className={globalStyles.separate_line}></div>
-                    <div style={{display:'flex', justifyContent:'space-around', marginTop:'30px', alignItems:'center'}}>
-                        <img className={styles.socialMedia} src={googleIcon} />
-                        <p className={globalStyles.simpleP}>{t('OR')}</p>
-                        <img className={styles.socialMedia} src={facebookIcon} />
-                    </div>
-                    <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:'30px'}}>
-                        <button style={{width:'300px'}} className={globalStyles.btn_border} onClick={()=> goSignUp()}> {t('New in Jobland? join now!') }</button>
+                    <div className={styles.languageDiv}>
+                        {UserStore.getLanguage() == 'en' ?
+                            <img src={he} className={styles.heLanguagePic} onClick={() => changeLanguage('he')} />
+                            :
+                            <img src={en} className={styles.enlanguagePic} onClick={() => changeLanguage('en')} />
+                        }
                     </div>
                 </div>
-               <img src={loginPicture}/>
-            </div>
-        </form>
-</>
+                {/*body*/}
+                <div className={styles.body}>
+                    <div className={styles.textForm}>
+                        <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', marginBottom: '30px' }}>
+                            <TextInputField type={'text'} placeHolder={t('Enter Your Email')} text={t('Email')} value={userEmail} onChange={handleInputChangeEmail} />
+                            <TextInputField type={'text'} placeHolder={t('Enter Your Password')} text={t('Password')} value={userPassword} onChange={handleInputChangePassword} />
+                        </div>
+                        <a className={globalStyles.mainSpan}>{t('Forgot your password?')}</a>
+                        <div style={{ marginTop: "15px", display: "flex", justifyContent: "center" }}>
+                            <button className={globalStyles.btn} onClick={login}> {t('Sign In')}</button>
+                        </div>
+                        <div style={{ marginTop: "40px" }} className={globalStyles.separate_line}></div>
+                        <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '30px', alignItems: 'center' }}>
+                            <img className={styles.socialMedia} src={googleIcon} />
+                            <p className={globalStyles.simpleP}>{t('OR')}</p>
+                            <img className={styles.socialMedia} src={facebookIcon} />
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px' }}>
+                            <button style={{ width: '300px' }} className={globalStyles.btn_border} onClick={() => goSignUp()}> {t('New in Jobland? join now!')}</button>
+                        </div>
+                    </div>
+                    <img src={loginPicture} />
+                </div>
+            </form>
+        </>
     );
-} )
+})
 
 
 
--- job-land-frontend/src/components/signIn/signIn.tsx
+++ job-land-frontend/src/components/signIn/signIn.tsx
@@ -1,8 +1,8 @@
-import {observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 import UserStore from '../../store/user';
-import {useTranslation} from "react-i18next";
-import React, {useState} from "react";
-import {toast} from "react-toastify";
+import { useTranslation } from "react-i18next";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 import styles from "../login/login.module.scss";
 import ToastComponent from "../../base-components/toaster/ToastComponent";
 import icon from "../../assets/images/icon.jpg";
@@ -13,15 +13,15 @@ import signupStyle from './signIn.module.scss'
 import TextInputField from "../../base-components/text-input/text-input-field";
 import googleIcon from "../../assets/images/social-media/google.png";
 import facebookIcon from "../../assets/images/social-media/facebook.png";
-import {useNavigate} from "react-router";
+import { useNavigate } from "react-router";
 import CryptoJS from 'crypto-js';
-import {Spinner} from "react-bootstrap";
+import { Spinner } from "react-bootstrap";
 
-const  SignIn  = observer( ()=>{
+const SignIn = observer(() => {
     //language
     const { t } = useTranslation();
     const { i18n } = useTranslation();
-    const changeLanguage = (lng:string) => {
+    const changeLanguage = (lng: string) => {
         UserStore.setLanguage(lng)
         i18n.changeLanguage(lng);
     };
@@ -29,37 +29,36 @@ const  SignIn  = observer( ()=>{
     const [activeRole, setactiveRole] = useState(false);
     //full name
     const [userName, setuserName] = useState('');
-    const handleInputChangeuserName = (value:string) => {
+    const handleInputChangeuserName = (value: string) => {
         setuserName(value);
     };
     //password
     const [userPassword, setUserPassword] = useState('');
-    const handleInputChangePassword = (value:string) => {
+    const handleInputChangePassword = (value: string) => {
         setUserPassword(value);
     };
     //email
     const [userEmail, setUserEmail] = useState('');
-    const handleInputChangeEmail = (value:string) => {
+    const handleInputChangeEmail = (value: string) => {
         setUserEmail(value);
     };
     //role
     const [role, setRole] = useState(0);
-    const handleInputChangeuserRole = (event:any,value:number) => {
+    const handleInputChangeuserRole = (event: any, value: number) => {
         event.preventDefault();
         setRole(value);
     };
     const secretKey = 'job-land'; //  secret key
-    const resetParameter=()=>{
+    const resetParameter = () => {
         setuserName('');
         setRole(0);
         setUserEmail('')
         setUserPassword('')
     }
-    const signup=async (event: any) => {
+    const signup = async (event: any) => {
         event.preventDefault()
-        if(userName.length==0 || userEmail.length==0 || userPassword.length==0)
-        {
-            toast.error(t('ERROR!')  +'One or more fields is empty' );
+        if (userName.length == 0 || userEmail.length == 0 || userPassword.length == 0) {
+            toast.error(t('ERROR!') + 'One or more fields is empty');
         }
         else {
             const encryptedPassword = CryptoJS.AES.encrypt(userPassword, secretKey).toString();
@@ -78,54 +77,56 @@ const  SignIn  = observer( ()=>{
 
         }
     }
-    const goLogin = ()=>{
+    const goLogin = () => {
         navigate('/login')
         UserStore.setSignedUp(true)
     }
     return (
         <>
             <ToastComponent />
-            {!UserStore.loading? (
-        <form className={styles.form} dir={ UserStore.getLanguage()=='en'?'ltr':'rtl'}>
-            <ToastComponent />
-            {/*header*/}
-            <div className={styles.formHeader}>
-                <img src={icon}  className={globalStyles.logoPic}/>
-                <div style={{display:'flex', flexDirection:'column'}}>
-                    <h2 className={styles.title} > {t('Join Jobland to find the right job' )} </h2>
-                </div>
-                <div className={styles.languageDiv}>
-                    { UserStore.getLanguage()=='en' ?
-                        <img src={he} className={styles.heLanguagePic} onClick={()=>changeLanguage('he')}/>
-                        :
-                        <img src={en} className={styles.enlanguagePic} onClick={()=>changeLanguage('en')}/>
-                    }
-                </div>
-            </div>
-            {/*body*/}
-            <div className={styles.body}>
-                <div className={styles.textForm}>
-                    <TextInputField type={'text'} placeHolder={t('Enter Your Full Name')} text={t('Full Name')} value={userName} onChange={handleInputChangeuserName}/>
-                    <TextInputField type={'text'} placeHolder={t('Enter Your Email')} text={t('Email')} value={userEmail} onChange={handleInputChangeEmail}/>
-                    <TextInputField type={'text'} placeHolder={t('Enter Your Password')} text={t('Password')} value={userPassword} onChange={handleInputChangePassword}/>
-                    <div style={{display:'flex', justifyContent:'space-around', alignItems:'center', gap:'20px'}}>
-                       <button style={{width:'150px'}} onClick={(e)=>handleInputChangeuserRole(e,0)} className={role==0?  globalStyles.btn : globalStyles.btn_border}>{t('Search job')}</button>
-                        <button style={{width:'150px'}} onClick={(e)=>handleInputChangeuserRole(e,1)} className={role==1?  globalStyles.btn : globalStyles.btn_border}>{t('Post job')}</button>
+            {!UserStore.loading ? (
+                <form className={styles.form} dir={UserStore.getLanguage() == 'en' ? 'ltr' : 'rtl'}>
+                    <ToastComponent />
+                    {/*header*/}
+                    <div className={styles.formHeader}>
+                        <img src={icon} className={globalStyles.logoPic} />
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <h2 className={styles.title} > {t('Join Jobland to find the right job')} </h2>
+                        </div>
+                        <div className={styles.languageDiv}>
+                            {UserStore.getLanguage() == 'en' ?
+                                <img src={he} className={styles.heLanguagePic} onClick={() => changeLanguage('he')} />
+                                :
+                                <img src={en} className={styles.enlanguagePic} onClick={() => changeLanguage('en')} />
+                            }
+                        </div>
                     </div>
-                    <div style={{marginTop:"35px", display:"flex", justifyContent:"center"}}>
-                        <button className={globalStyles.btn}  onClick={signup}> {t('Sign Up') }</button>
-                    </div>
-                    <div style={{marginTop:"40px"}} className={globalStyles.separate_line}></div>
+                    {/*body*/}
+                    <div className={styles.body}>
+                        <div className={styles.textForm}>
+                            <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', marginBottom: '30px' }}>
+                                <TextInputField type={'text'} placeHolder={t('Enter Your Full Name')} text={t('Full Name')} value={userName} onChange={handleInputChangeuserName} />
+                                <TextInputField type={'text'} placeHolder={t('Enter Your Email')} text={t('Email')} value={userEmail} onChange={handleInputChangeEmail} />
+                                <TextInputField type={'text'} placeHolder={t('Enter Your Password')} text={t('Password')} value={userPassword} onChange={handleInputChangePassword} />
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', gap: '20px' }}>
+                                <button style={{ width: '150px' }} onClick={(e) => handleInputChangeuserRole(e, 0)} className={role == 0 ? globalStyles.btn : globalStyles.btn_border}>{t('Search job')}</button>
+                                <button style={{ width: '150px' }} onClick={(e) => handleInputChangeuserRole(e, 1)} className={role == 1 ? globalStyles.btn : globalStyles.btn_border}>{t('Post job')}</button>
+                            </div>
+                            <div style={{ marginTop: "35px", display: "flex", justifyContent: "center" }}>
+                                <button className={globalStyles.btn} onClick={signup}> {t('Sign Up')}</button>
+                            </div>
+                            <div style={{ marginTop: "40px" }} className={globalStyles.separate_line}></div>
 
-                    <div style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:'30px'}}>
-                        <button style={{width:'300px'}} className={globalStyles.btn_border} onClick={()=> goLogin()}> {t('Already on Jobland? Sign in') }</button>
+                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px' }}>
+                                <button style={{ width: '300px' }} className={globalStyles.btn_border} onClick={() => goLogin()}> {t('Already on Jobland? Sign in')}</button>
+                            </div>
+                        </div>
+                        <div className={signupStyle.logoPicture}></div>
                     </div>
-                </div>
-                <div className={signupStyle.logoPicture}></div>
-            </div>
-        </form>
-            ):(<Spinner/>)}
-            </>
+                </form>
+            ) : (<Spinner />)}
+        </>
     );
-} )
+})
 export default SignIn
